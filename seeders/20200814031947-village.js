17bcf34081af86db63b796399c9463645f397634
D-Brewst <devonbrewster31@gmail.com>
2020-08-14 10:06:16 -0400

    fixed login page and fixed some errors in html and css files

--- seeders/20200814031947-village.js
+++ seeders/20200814031947-village.js
@@ -1,13 +1,24 @@
 'use strict';
+var db = require("../models");
+var bcrypt = require("bcryptjs");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     
-     
+    //  db.User.create({
+    //   username: 'JohnDoe',
+    //   email: 'happy@gmail.com',
+    //   password: 'super',
+    //  })
      await queryInterface.bulkInsert('Users', [{
        username: 'JohnDoe',
        email: 'happy@gmail.com',
-       password: 'super',
+       password: bcrypt.hashSync(
+        "super",
+        bcrypt.genSaltSync(10),
+        null
+      )
+      ,
        createdAt: new Date(),
        updatedAt: new Date()
       }], {});
