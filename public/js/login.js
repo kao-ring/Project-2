66146d717ad6a19ab2de4db27beee086b01ccf3b
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-14 10:21:47 -0400

    Fixed index.html page

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Getting references to our form and inputs
   var loginForm = $("form.login");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+  loginForm.on("submit", function (event) {
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
@@ -25,14 +25,14 @@ $(document).ready(function() {
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
   function loginUser(email, password) {
     $.post("/api/login", {
-      email: email,
-      password: password
-    })
-      .then(function() {
+        email: email,
+        password: password
+      })
+      .then(function () {
         window.location.replace("/members");
         // If there's an error, log the error
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
       });
   }
