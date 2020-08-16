be09ea6f791700951c5dc63cc8134dc826e725b9
Joshua Wilensky <joshwilensky@gmail.com>
2020-08-16 13:54:30 -0400

    made changes to the signup pages

--- config/config.js
+++ config/config.js
@@ -1,13 +1,8 @@
 module.exports = {
   development: {
     username: "root",
-<<<<<<< HEAD
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-=======
-    password: "trilogy!",
-    database: "village_db",
->>>>>>> 009a5d92598a182498f9fda2609bafb5d6c80507
     host: "127.0.0.1",
     dialect: "mysql",
   },
